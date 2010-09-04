288d0b649e567c270f1b6347896653bd364e094b
Jim Huang <jserv@0xlab.org>
2010-09-04 21:16:19 +0800

    Move machine-dependent definitions from jni.h to jni_md.h
    Besides the typedefs like "jint", JNIEXPORT and JNICALL are preprocessor
    defined constants specific to each platform.  They are now moved into
    header file <jni_md.h>.
    
    Change-Id: I8887eaf08a26e9f2f71ff3f481f5b758901aaca4

--- libnativehelper/include/nativehelper/jni.h
+++ libnativehelper/include/nativehelper/jni.h
@@ -11,29 +11,8 @@
 
 #include <stdarg.h>
 
-/*
- * Primitive types that match up with Java equivalents.
- */
-#ifdef HAVE_INTTYPES_H
-# include <inttypes.h>      /* C99 */
-typedef uint8_t         jboolean;       /* unsigned 8 bits */
-typedef int8_t          jbyte;          /* signed 8 bits */
-typedef uint16_t        jchar;          /* unsigned 16 bits */
-typedef int16_t         jshort;         /* signed 16 bits */
-typedef int32_t         jint;           /* signed 32 bits */
-typedef int64_t         jlong;          /* signed 64 bits */
-typedef float           jfloat;         /* 32-bit IEEE 754 */
-typedef double          jdouble;        /* 64-bit IEEE 754 */
-#else
-typedef unsigned char   jboolean;       /* unsigned 8 bits */
-typedef signed char     jbyte;          /* signed 8 bits */
-typedef unsigned short  jchar;          /* unsigned 16 bits */
-typedef short           jshort;         /* signed 16 bits */
-typedef int             jint;           /* signed 32 bits */
-typedef long long       jlong;          /* signed 64 bits */
-typedef float           jfloat;         /* 32-bit IEEE 754 */
-typedef double          jdouble;        /* 64-bit IEEE 754 */
-#endif
+/* machine-dependent definition for JNI primitive types. */
+#include "jni_md.h"
 
 /* "cardinal indices and sizes" */
 typedef jint            jsize;
@@ -1131,10 +1110,5 @@ void JNI_OnUnload(JavaVM* vm, void* reserved);
 
 #define JNI_COMMIT      1           /* copy content, do not free buffer */
 #define JNI_ABORT       2           /* free buffer w/o copying back */
-
-/* need these for Windows-aware headers */
-#define JNIIMPORT
-#define JNIEXPORT
-#define JNICALL
 
 #endif /*_JNI_H*/
---
+++ libnativehelper/include/nativehelper/jni_md.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2010 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Everything here is expected to be platform-specific.
+ */
+
+#ifndef _ANDROID_JNI_MD_H_
+#define _ANDROID_JNI_MD_H_
+
+/*
+ * Primitive types that match up with Java equivalents.
+ */
+#ifdef HAVE_INTTYPES_H
+# include <inttypes.h>      /* C99 */
+typedef uint8_t         jboolean;       /* unsigned 8 bits */
+typedef int8_t          jbyte;          /* signed 8 bits */
+typedef uint16_t        jchar;          /* unsigned 16 bits */
+typedef int16_t         jshort;         /* signed 16 bits */
+typedef int32_t         jint;           /* signed 32 bits */
+typedef int64_t         jlong;          /* signed 64 bits */
+typedef float           jfloat;         /* 32-bit IEEE 754 */
+typedef double          jdouble;        /* 64-bit IEEE 754 */
+#else
+typedef unsigned char   jboolean;       /* unsigned 8 bits */
+typedef signed char     jbyte;          /* signed 8 bits */
+typedef unsigned short  jchar;          /* unsigned 16 bits */
+typedef short           jshort;         /* signed 16 bits */
+typedef int             jint;           /* signed 32 bits */
+typedef long long       jlong;          /* signed 64 bits */
+typedef float           jfloat;         /* 32-bit IEEE 754 */
+typedef double          jdouble;        /* 64-bit IEEE 754 */
+#endif
+
+#define JNIEXPORT
+#define JNIIMPORT
+#define JNICALL
+
+#endif /* ! _ANDROID_JNI_MD_H_ */
