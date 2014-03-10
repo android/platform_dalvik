378f6ed72d07623f41748c0be371c42b8432dc43
Narayan Kamath <narayan@google.com>
2014-03-10 11:26:36 +0000

    Add native binding for isDexOptNeededInternal.
    Dalvik has no profiler, so we can safely ignore the
    "defer" argument.
    
    Keeps dalvik functional on master.
    
    Change-Id: I2ace9a215e7ff471294927aa870e55cf7c0327b1

--- vm/native/dalvik_system_DexFile.cpp
+++ vm/native/dalvik_system_DexFile.cpp
@@ -514,6 +514,11 @@ static void Dalvik_dalvik_system_DexFile_isDexOptNeeded(const u4* args,
     }
 }
 
+static void Dalvik_dalvik_system_DexFile_isDexOptNeededInternal(const u4*args,
+    JValue* pResult) {
+    return Dalvik_dalvik_system_DexFile_isDexOptNeeded(args, pResult);
+}
+
 const DalvikNativeMethod dvm_dalvik_system_DexFile[] = {
     { "openDexFileNative",  "(Ljava/lang/String;Ljava/lang/String;I)J",
         Dalvik_dalvik_system_DexFile_openDexFileNative },
@@ -526,6 +531,8 @@ const DalvikNativeMethod dvm_dalvik_system_DexFile[] = {
     { "getClassNameList",   "(J)[Ljava/lang/String;",
         Dalvik_dalvik_system_DexFile_getClassNameList },
     { "isDexOptNeeded",     "(Ljava/lang/String;)Z",
+        Dalvik_dalvik_system_DexFile_isDexOptNeeded },
+    { "isDexOptNeededInternal",     "(Ljava/lang/String;Ljava/lang/String;Z)Z",
         Dalvik_dalvik_system_DexFile_isDexOptNeeded },
     { NULL, NULL, NULL },
 };
