2776faecca83b0732043f9b68ec19f6f3ce84fd2
Benoit Lamarche <benoitlamarche@google.com>
2014-04-18 18:14:27 +0200

    Return the correct error code in multi-thread mode
    Bug: 14062905
    
    In "--num-threads" mode, dx did not return a result code different
    than zero in case of a Throwable.
    This CL aims to have the same behaviour in multi-threaded mode than
    in normal mode.
    There are 2 cases:
    - When there is an Exception, it is supposed to be handled by
    ClassPathOpener.Consumer.onException(). The number of errors is then
    incremented, which is used in "com.android.dx.command.dexer.Main" to
    return the error code. With "--num-threads" it couldn't be handled by
    onException(). So I've modified the ParallelProcessor to use the
    ClassPathOpener, so that the Exception is handled inside the thread.
    - When there is an Error it is handled using the Futures of
    ParallelProcessors through ExecutorService.
    
    Change-Id: Idc9d8d8bc737587ca518f8f9166e5e511ec22908

--- dx/src/com/android/dx/command/dexer/Main.java
+++ dx/src/com/android/dx/command/dexer/Main.java
@@ -64,8 +64,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 import java.util.jar.Attributes;
 import java.util.jar.JarEntry;
@@ -151,6 +154,8 @@ public class Main {
     /** number of errors during processing */
     private static int errors = 0;
 
+    private static List<Future<?>> parallelProcessorFutures;
+
     /** {@code non-null;} parsed command-line arguments */
     private static Arguments args;
 
@@ -170,7 +175,7 @@ public class Main {
     private static ExecutorService threadPool;
 
     /** true if any files are successfully processed */
-    private static boolean anyFilesProcessed;
+    private static volatile boolean anyFilesProcessed;
 
     /** class files older than this must be defined in the target dex file. */
     private static long minimumFileAge = 0;
@@ -461,6 +466,7 @@ public class Main {
 
         if (args.numThreads > 1) {
             threadPool = Executors.newFixedThreadPool(args.numThreads);
+            parallelProcessorFutures = new ArrayList<Future<?>>();
         }
 
         try {
@@ -471,9 +477,7 @@ public class Main {
 
                 // forced in main dex
                 for (int i = 0; i < fileNames.length; i++) {
-                    if (processOne(fileNames[i], mainPassFilter)) {
-                        anyFilesProcessed = true;
-                    }
+                    processOne(fileNames[i], mainPassFilter);
                 }
 
                 if (dexOutputArrays.size() > 1) {
@@ -488,16 +492,12 @@ public class Main {
 
                 // remaining files
                 for (int i = 0; i < fileNames.length; i++) {
-                    if (processOne(fileNames[i], new NotFilter(mainPassFilter))) {
-                        anyFilesProcessed = true;
-                    }
+                    processOne(fileNames[i], new NotFilter(mainPassFilter));
                 }
             } else {
                 // without --main-dex-list
                 for (int i = 0; i < fileNames.length; i++) {
-                    if (processOne(fileNames[i], ClassPathOpener.acceptAll)) {
-                        anyFilesProcessed = true;
-                    }
+                    processOne(fileNames[i], ClassPathOpener.acceptAll);
                 }
             }
         } catch (StopProcessing ex) {
@@ -511,8 +511,20 @@ public class Main {
             try {
                 threadPool.shutdown();
                 threadPool.awaitTermination(600L, TimeUnit.SECONDS);
+                for (Future<?> future : parallelProcessorFutures) {
+                    future.get();
+                }
             } catch (InterruptedException ex) {
                 throw new RuntimeException("Timed out waiting for threads.");
+            } catch (ExecutionException e) {
+              Throwable cause = e.getCause();
+              // All Exceptions should have been handled in the ParallelProcessor, only Errors
+              // should remain
+              if (cause instanceof Error) {
+                throw (Error) e.getCause();
+              } else {
+                throw new AssertionError(e.getCause());
+              }
             }
         }
 
@@ -557,21 +569,19 @@ public class Main {
      * be the path of a class file, a jar file, or a directory
      * containing class files.
      * @param filter {@code non-null;} A filter for excluding files.
-     * @return whether any processing actually happened
      */
-    private static boolean processOne(String pathname, FileNameFilter filter) {
+    private static void processOne(String pathname, FileNameFilter filter) {
         ClassPathOpener opener;
 
         opener = new ClassPathOpener(pathname, false, filter,
                 new ClassPathOpener.Consumer() {
+
+            @Override
             public boolean processFileBytes(String name, long lastModified, byte[] bytes) {
-                if (args.numThreads > 1) {
-                    threadPool.execute(new ParallelProcessor(name, lastModified, bytes));
-                    return false;
-                } else {
-                    return Main.processFileBytes(name, lastModified, bytes);
-                }
+                return Main.processFileBytes(name, lastModified, bytes);
             }
+
+            @Override
             public void onException(Exception ex) {
                 if (ex instanceof StopProcessing) {
                     throw (StopProcessing) ex;
@@ -585,6 +595,8 @@ public class Main {
                 }
                 errors++;
             }
+
+            @Override
             public void onProcessArchiveStart(File file) {
                 if (args.verbose) {
                     DxConsole.out.println("processing archive " + file +
@@ -593,7 +605,13 @@ public class Main {
             }
         });
 
-        return opener.process();
+        if (args.numThreads > 1) {
+            parallelProcessorFutures.add(threadPool.submit(new ParallelProcessor(opener)));
+        } else {
+            if (opener.process()) {
+                anyFilesProcessed = true;
+            }
+        }
     }
 
     /**
@@ -1521,29 +1539,15 @@ public class Main {
     /** Runnable helper class to process files in multiple threads */
     private static class ParallelProcessor implements Runnable {
 
-        String path;
-        long lastModified;
-        byte[] bytes;
+        ClassPathOpener classPathOpener;
 
-        /**
-         * Constructs an instance.
-         *
-         * @param path {@code non-null;} filename of element. May not be a valid
-         * filesystem path.
-         * @param bytes {@code non-null;} file data
-         */
-        private ParallelProcessor(String path, long lastModified, byte bytes[]) {
-            this.path = path;
-            this.lastModified = lastModified;
-            this.bytes = bytes;
+        private ParallelProcessor(ClassPathOpener classPathOpener) {
+            this.classPathOpener = classPathOpener;
         }
 
-        /**
-         * Task run by each thread in the thread pool. Runs processFileBytes
-         * with the given path and bytes.
-         */
+        @Override
         public void run() {
-            if (Main.processFileBytes(path, lastModified, bytes)) {
+            if (classPathOpener.process()) {
                 anyFilesProcessed = true;
             }
         }
