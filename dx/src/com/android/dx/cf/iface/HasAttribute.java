094fe665e15c5105fcca6314bbe0f55bfd48b924
Yohann Roussel <yroussel@google.com>
2014-11-18 19:15:02 +0100

    Keep annotated classes in main dex list .
    Only classes annotated with a runtime visible annotation are to be kept.
    See https://code.google.com/p/android/issues/detail?id=78144
    
    Bug: 18385117
    Change-Id: Id377b3945b22201ebad05e7cd026af57acc60292

---
+++ dx/src/com/android/dx/cf/iface/HasAttribute.java
@@ -0,0 +1,16 @@
+package com.android.dx.cf.iface;
+
+/**
+ * An element that can have {@link Attribute}
+ */
+public interface HasAttribute {
+
+    /**
+     * Get the element {@code attributes} (along with
+     * {@code attributes_count}).
+     *
+     * @return {@code non-null;} the attributes list
+     */
+    public AttributeList getAttributes();
+
+}
