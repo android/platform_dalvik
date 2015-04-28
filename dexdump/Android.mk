5844f063e659b2e3585dd23c89e6ac6c072f67fe
Narayan Kamath <narayan@google.com>
2015-04-28 10:18:54 +0100

    Add a libbase dependency for dexdump/dexlist.
    Required by the libziparchive static dependency.
    
    Change-Id: Iaa7aad6e7f3dfc6014651385d7182c009e182914

--- dexdump/Android.mk
+++ dexdump/Android.mk
@@ -23,8 +23,14 @@ dexdump_src_files := \
 dexdump_c_includes := \
 		dalvik
 
+dexdump_static_libraries_sdk := \
+		libdex \
+		libbase
+
 dexdump_static_libraries := \
-		libdex
+                $(dexdump_static_libraries_sdk) \
+		libutils \
+		liblog
 
 ##
 ##
@@ -38,7 +44,7 @@ LOCAL_MODULE := dexdump
 LOCAL_SRC_FILES := $(dexdump_src_files)
 LOCAL_C_INCLUDES := $(dexdump_c_includes)
 LOCAL_SHARED_LIBRARIES := libz liblog libutils
-LOCAL_STATIC_LIBRARIES := $(dexdump_static_libraries)
+LOCAL_STATIC_LIBRARIES := $(dexdump_static_libraries_sdk)
 LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
 LOCAL_MODULE_TAGS := optional
 LOCAL_LDLIBS +=
@@ -50,7 +56,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := libdexdump_static
 LOCAL_SRC_FILES := $(dexdump_src_files)
 LOCAL_C_INCLUDES := $(dexdump_c_includes)
-LOCAL_STATIC_LIBRARIES := $(dexdump_static_libraries)
+LOCAL_STATIC_LIBRARIES := $(dexdump_static_libraries_sdk)
 LOCAL_SHARED_LIBRARIES := libutils
 LOCAL_MODULE_TAGS := optional
 LOCAL_32_BIT_ONLY := true
@@ -70,7 +76,7 @@ LOCAL_MODULE := dexdump
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(dexdump_src_files)
 LOCAL_C_INCLUDES := $(dexdump_c_includes)
-LOCAL_STATIC_LIBRARIES := $(dexdump_static_libraries) liblog libutils
+LOCAL_STATIC_LIBRARIES := $(dexdump_static_libraries)
 ifneq ($(strip $(USE_MINGW)),)
 LOCAL_STATIC_LIBRARIES += libz
 else
