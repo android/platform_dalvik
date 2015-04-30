79412b540ffcfdd2e5d4efa7a9815f40bbb05bb8
Benoit Lamarche <benoitlamarche@google.com>
2015-04-30 16:48:29 +0200

    Add support for multi-dex as libraries
    Contributed by lberki@google.com
    
    Change-Id: I881363ae87477901533c79df5dedc21c04deee05

--- dx/src/com/android/dx/command/dexer/Main.java
+++ dx/src/com/android/dx/command/dexer/Main.java
@@ -423,11 +423,16 @@ public class Main {
     }
 
     private static String getDexFileName(int i) {
+        String dexFileName;
         if (i == 0) {
-            return DexFormat.DEX_IN_JAR_NAME;
+            dexFileName = DexFormat.DEX_IN_JAR_NAME;
         } else {
-            return DEX_PREFIX + (i + 1) + DEX_EXTENSION;
+            dexFileName = DEX_PREFIX + (i + 1) + DEX_EXTENSION;
         }
+        if (!DexFormat.MULTIDEX_IN_JAR_RX.matcher(dexFileName).matches()) {
+            throw new AssertionError("Invalid dex name");
+        }
+        return dexFileName;
     }
 
     private static void readPathsFromFile(String fileName, Collection<String> paths) throws IOException {
@@ -689,7 +694,7 @@ public class Main {
     private static boolean processFileBytes(String name, long lastModified, byte[] bytes) {
 
         boolean isClass = name.endsWith(".class");
-        boolean isClassesDex = name.equals(DexFormat.DEX_IN_JAR_NAME);
+        boolean isClassesDex = DexFormat.MULTIDEX_IN_JAR_RX.matcher(name).matches();
         boolean keepResources = (outputResources != null);
 
         if (!isClass && !isClassesDex && !keepResources) {
