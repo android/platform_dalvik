82ffbbd6024d49aed200a5f0017e70c00990d325
Orion Hodson <oth@google.com>
2017-04-10 15:43:28 +0100

    Fix missing receiver type information in invoke-custom
    Bug:  36957105
    Test: manual using the dex file generated by dx/tests/135-invoke-custom
    Change-Id: I28db8c013a63cebce8a6202dc3241bb494153b80

--- dexdump/DexDump.cpp
+++ dexdump/DexDump.cpp
@@ -1866,30 +1866,45 @@ static void dumpMethodHandles(DexFile* pDexFile)
     for (u4 i = 0; i < item->size; ++i) {
         const DexMethodHandleItem& mh = method_handles[i];
         bool is_invoke = false;
-        const char* type;
-        switch (mh.methodHandleType) {
-            case 0:
+        bool is_instance = false;
+        const char* type = "";
+        switch ((MethodHandleType) mh.methodHandleType) {
+            case MethodHandleType::STATIC_PUT:
                 type = "put-static";
                 break;
-            case 1:
+            case MethodHandleType::STATIC_GET:
                 type = "get-static";
                 break;
-            case 2:
+            case MethodHandleType::INSTANCE_PUT:
                 type = "put-instance";
+                is_instance = true;
                 break;
-            case 3:
+            case MethodHandleType::INSTANCE_GET:
                 type = "get-instance";
+                is_instance = true;
                 break;
-            case 4:
+            case MethodHandleType::INVOKE_STATIC:
                 type = "invoke-static";
                 is_invoke = true;
                 break;
-            case 5:
+            case MethodHandleType::INVOKE_INSTANCE:
                 type = "invoke-instance";
+                is_instance = true;
                 is_invoke = true;
                 break;
-            case 6:
+            case MethodHandleType::INVOKE_CONSTRUCTOR:
                 type = "invoke-constructor";
+                is_instance = true;
+                is_invoke = true;
+                break;
+            case MethodHandleType::INVOKE_DIRECT:
+                type = "invoke-direct";
+                is_instance = true;
+                is_invoke = true;
+                break;
+            case  MethodHandleType::INVOKE_INTERFACE:
+                type = "invoke-interface";
+                is_instance = true;
                 is_invoke = true;
                 break;
         }
@@ -1902,18 +1917,24 @@ static void dumpMethodHandles(DexFile* pDexFile)
             getFieldInfo(pDexFile, mh.fieldOrMethodIdx, &info);
         }
 
+        const char* instance = "";
+        if (is_instance) {
+          instance = info.classDescriptor;
+        }
+
         if (gOptions.outputFormat == OUTPUT_XML) {
             printf("<method_handle index index=\"%u\"\n", i);
             printf(" type=\"%s\"\n", type);
             printf(" target_class=\"%s\"\n", info.classDescriptor);
             printf(" target_member=\"%s\"\n", info.name);
-            printf(" target_member_type=\"%s\"\n", info.signature);
+            printf(" target_member_type=\"%c%s%s\"\n",
+                   info.signature[0], instance, info.signature + 1);
             printf("</method_handle>\n");
         } else {
             printf("Method Handle #%u:\n", i);
             printf("  type        : %s\n", type);
             printf("  target      : %s %s\n", info.classDescriptor, info.name);
-            printf("  target_type : %s\n", info.signature);
+            printf("  target_type : %c%s%s\n", info.signature[0], instance, info.signature + 1);
         }
     }
 }
