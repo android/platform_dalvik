5415c85783744a5ac25c749ced0bbce6a0515749
Orion Hodson <oth@google.com>
2017-06-13 10:15:32 +0100

    dx: Add support for invoke-custom
    This change adds the translations to invoke-custom and extends the DEX
    file sections that dx emits sections for method handles and call
    sites.
    
    Test: dx/tests/run-test 135
    Bug: 36641082
    Bug: 36957105
    Change-Id: Ia1c5b548755485755e8bab0a8f99bd745c2960cd

--- dx/src/com/android/dx/cf/code/Simulator.java
+++ dx/src/com/android/dx/cf/code/Simulator.java
@@ -23,6 +23,7 @@ import com.android.dx.rop.cst.Constant;
 import com.android.dx.rop.cst.CstFieldRef;
 import com.android.dx.rop.cst.CstInteger;
 import com.android.dx.rop.cst.CstInterfaceMethodRef;
+import com.android.dx.rop.cst.CstInvokeDynamic;
 import com.android.dx.rop.cst.CstMethodRef;
 import com.android.dx.rop.cst.CstType;
 import com.android.dx.rop.type.Prototype;
@@ -700,6 +701,18 @@ public class Simulator {
                     boolean staticMethod = (opcode == ByteOps.INVOKESTATIC);
                     Prototype prototype =
                         ((CstMethodRef) cst).getPrototype(staticMethod);
+                    machine.popArgs(frame, prototype);
+                    break;
+                }
+                case ByteOps.INVOKEDYNAMIC: {
+                    if (!dexOptions.canUseInvokeCustom()) {
+                        throw new SimException(
+                            "invalid opcode " + Hex.u1(opcode) +
+                            " (invokedynamic requires --min-sdk-version >= " +
+                            DexFormat.API_INVOKE_POLYMORPHIC + ")");
+                    }
+                    CstInvokeDynamic invokeDynamic = (CstInvokeDynamic) cst;
+                    Prototype prototype = invokeDynamic.getPrototype();
                     machine.popArgs(frame, prototype);
                     break;
                 }
